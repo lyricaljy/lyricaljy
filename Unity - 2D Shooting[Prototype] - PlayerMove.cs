b52efcda4f5cc98233d29339dced4ad355f9210f
lyricaljy <105025984+lyricaljy@users.noreply.github.com>
2022-05-10 20:57:13 +0900

    Update Unity - 2D Shooting[Prototype] - PlayerMove.cs

--- Unity - 2D Shooting[Prototype] - PlayerMove.cs
+++ Unity - 2D Shooting[Prototype] - PlayerMove.cs
@@ -1,1 +1,20 @@
 public class PlayerMove : MonoBehaviour
+{
+  void Start()
+  {
+  
+  }
+  
+  public float speed = 5;
+  
+  void Update()
+  {
+    float h = Input.GetAxis("Horizontal");
+    float v = Input.GetAxis("Vertical");
+    
+    Vector3 dir = new Vector3(h, v, 0);
+    dir.Normalize();
+    
+    transform.position += dir * speed * Time.deltaTime;
+   }
+ }
